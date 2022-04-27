783ccb1509a8918b8d79327f8e37fd5107dcd7a3
Loukmane Oustani <61042572+L2oukmane@users.noreply.github.com>
2022-04-27 13:36:51 +0300

    Update styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,22 +14,22 @@ class Calculator extends React.Component {
         <button type="button" className="c-item">AC</button>
         <button type="button" className="c-item">+/-</button>
         <button type="button" className="c-item">%</button>
-        <button type="button" className="c-item bg-blue">÷</button>
+        <button type="button" className="c-item bg-orange">÷</button>
         <button type="button" className="c-item">7</button>
         <button type="button" className="c-item">8</button>
         <button type="button" className="c-item">9</button>
-        <button type="button" className="c-item bg-blue">x</button>
+        <button type="button" className="c-item bg-orange">x</button>
         <button type="button" className="c-item">4</button>
         <button type="button" className="c-item">5</button>
         <button type="button" className="c-item">6</button>
-        <button type="button" className="c-item bg-blue">-</button>
+        <button type="button" className="c-item bg-orange">-</button>
         <button type="button" className="c-item">1</button>
         <button type="button" className="c-item">2</button>
         <button type="button" className="c-item">3</button>
-        <button type="button" className="c-item bg-blue">+</button>
+        <button type="button" className="c-item bg-orange">+</button>
         <button type="button" className="c-item zero">0</button>
         <button type="button" className="c-item">.</button>
-        <button type="button" className="c-item bg-blue">=</button>
+        <button type="button" className="c-item bg-orange">=</button>
       </div>
     );
   }
