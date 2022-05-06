39a5dca31412d61ef32e3f471043ebd38de330e0
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-06 11:16:10 +0300

    use router to render pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import CalculatorPage from './pages/CalculatorPage';
+import Quotes from './pages/Quotes';
+import NotMatch from './pages/NotMatch';
 import './App.css';
-import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="main-container">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<CalculatorPage />} />
+      <Route path="/quote" element={<Quotes />} />
+      <Route path="*" element={<NotMatch />} />
+    </Routes>
+  </>
+);
 
 export default App;
