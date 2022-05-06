ce135ea98a0083f28a942bfbd669f92beece3595
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-06 11:14:20 +0300

    Add quotes

---
+++ src/pages/Quotes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Quotes = () => (
+  <section className="quote">
+    <div className="hello">
+      <div style={{ fontSize: '20px', textAlign: 'center' }}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi magni
+        quidem asperiores odio similique fugit consequuntur, sapiente inventore
+        accusantium distinctio?
+      </div>
+    </div>
+  </section>
+);
+
+export default Quotes;
